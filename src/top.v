683a615f50c24cd9f463784beb351caa5c7f77e5
Ale6896 <ale6896@gmail.com>
2023-08-31 07:54:06 -0600

    Avance

--- src/top.v
+++ src/top.v
@@ -12,36 +12,43 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
 );
 
     //wire reset = ! rst_n;
-    reg [7:0] ALU_out, REG_In;
+    reg [7:0] ALU_Out, REG_In;
     wire [7:0] REG_A, REG_B;
     reg [1:0] addr_A, addr_B, addr_In;
     wire [3:0] ENC_In;
     reg [3:0] ENC_Out;
-    wire [2:0] OP;
-    wire EN, ENC_key_p;
+    wire [1:0] OP;
+    wire EN, ENC_key_p, CarryOut;
     wire [1:0] count;
 
 
     // uses 4 bits input for keyboard code and 3 bits for operation
     assign ui_in[3:0] = ENC_In;
-    assign ui_int[7:4] = OP;
+    assign ui_int[5:4] = OP;
 
-    assign uo_out[7:0] = ALU_out;
+    // assing the dedicated output to ALU Out
+    assign uo_out[7:0] = ALU_Out;
 
     // use 7 bidirectionals as inputs and 1 as output
     assign uio_oe = 8'b10000000;
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_in[5:0] = reg_address[5:0];
+    // assign the input/output pins
+    assign uio_in[5:0] = {addr_In, addr_B, addr_A};
     assign uio_in[6] = EN;
-    assign uio_out = zero_flag;
+    assign uio_out[7] = zero_flag;
 
-    // Extends 4 bit encoder out to 8 bit by adding 0's
-    assign REG_In = {4'b0, ENC_Out};
+   
 
     always @(posedge clk) begin
         
+        // Extends 4 bit encoder out to 8 bit by adding 0's
+        REG_In <= {4'b0, ENC_Out};
+
     end
+
+    _2b_counter counter(.clk(clk),
+                        .count(count)
+                        );
 
     // instantiate encoder
     encoder encoder(.keyboard(ENC_In),
@@ -52,11 +59,22 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
                     );
 
     // instantiate Register Bank
-    REG REG(.DIR_A(addr_A), .DIR_B(addr_B), .DIR_WR(addr_In),
+    REG REG(.DIR_A(addr_A),
+            .DIR_B(addr_B),
+            .DIR_WR(addr_In),
             .DI(REG_In),
             .clk(clk),
             .EN(EN),
-            .DOA(REG_A), .DOB(REG_B)
+            .DOA(REG_A),
+            .DOB(REG_B)
+            );
+
+    ALU ALU(.A(REG_A),
+            .B(REG_B),
+            .ALU_Sel(OP),
+            .ALU_Out(ALU_Out),
+            .CarryOut(CarryOut),
+            .ZeroFlag(zero_flag)
             );
 
 
