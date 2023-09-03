1165369788e1145128ad611c0f77d15e9b97264a
Iosaias <99456315+Iosaias@users.noreply.github.com>
2023-09-03 16:43:42 -0600

    Update encoder.v

--- src/encoder.v
+++ src/encoder.v
@@ -22,7 +22,7 @@
 
 module encoder( input [3:0] keyboard,
                 input clock,
-                output reg[3:0] hex_out,
+                output [3:0] hex_out,
                 input [1:0] counter,
                 
     );
