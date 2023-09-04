16551bf99a453da6a3ca90b0eb35ea8fd0591484
Iosaias <99456315+Iosaias@users.noreply.github.com>
2023-09-03 21:32:50 -0600

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -25,8 +25,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     
 
     // use 7 bidirectionals as inputs and 1 as output
-    always_comb
-    begin
+
     assign uio_oe = 8'b10110000;
     assign ENC_In=uio_in[3:0];
     assign OP=ui_in[1:0];
@@ -34,7 +33,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     assign addr_In=ui_in[3:2] ;
     assign addr_A=ui_in[5:4] ;
     assign addr_B=ui_in[7:6] ;
-     ssign EN=uio_in[6] ;
+    assign EN=uio_in[6] ;
     count_out=count;
     // uses 4 bits input for keyboard code and 3 bits for operation
    
@@ -44,11 +43,11 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
            // assign the input/output pins
 
    // assign uio_out[7] = zero_flag;
-            assign uio_out ={zero_flag,0,count_out,3'b000};
+    assign uio_out ={zero_flag,0,count_out,3'b000};
       
      // Extends 4 bit encoder out to 8 bit by adding 0's
         REG_In = {4'b0, ENC_Out};
-        end
+        
 
     always @(posedge clk) begin
 
