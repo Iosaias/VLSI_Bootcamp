ea18e00f9cd7f0977c980e31c598f5ecd8b3b92c
Iosaias <99456315+Iosaias@users.noreply.github.com>
2023-09-04 17:42:39 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,18 +1,18 @@
-import cocotb
+cimport cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
-async def dummy_test(dut):
+async def tt_um_losaias(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-
+    dut.uio_in=8'b01001110
+    dut.ui_in.value = 8'b00000010	
     # reset
     dut._log.info("reset")
     dut.rst_n.value = 0
     # set the compare value
     dut.ui_in.value = 1
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1  
-
+    dut.rst_n.value = 1 
