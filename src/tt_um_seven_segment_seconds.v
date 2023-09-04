64712010c893a388e6ecd31d873fba9a21b58d0b
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2023-09-04 12:07:49 -0600

    fix

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -22,7 +22,6 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     wire EN, CarryOut;
     
     
-    
 
     // use 7 bidirectionals as inputs and 1 as output
 
@@ -35,6 +34,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     assign addr_B=ui_in[7:6] ;
     assign EN=uio_in[6] ;
     assign count_out=count;
+    assign uo_out[7:0]=ALU_Out[7:0];
     // uses 4 bits input for keyboard code and 3 bits for operation
    
     // assing the dedicated output to ALU Out
